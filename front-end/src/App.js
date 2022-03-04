647b4c3475fff70e1184e283733977e1d586c9c5
nakamaycoders <nakamaycoders@gmail.com>
2022-03-05 03:01:27 +0500

    ok

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import {BrowserRouter as Router } from "react-router-dom";
-// import Cards2 from "./components/layout/Cards2";
-// import { Header } from './components/layout/Header';
-// import Footer from "./components/layout/Footer";
+
 import HomeScreen from './components/HomeScreen';
-// import Cardss from './components/layout/Cardss';
-// import { Home } from './components/Page/Home';
+
 import { BrowserRouter ,Route, Switch} from 'react-router-dom';
-// import { Nav } from 'react-bootstrap';
+
 import Footer from './components/layout/Footer';
 import { Navbar } from './components/layout/Navbar';
 import {Nav} from './components/layout/Nav'
 import Calculator from './Financing/Calculator';
+
+import AboutUs from './components/layout/AboutUs';
 
 
 function App() {
@@ -37,7 +35,7 @@ function App() {
     
      </Switch>
 
-   
+    <AboutUs/>
      <Footer/>
      
     </div>
--- front-end/src/components/HomeScreen.js
+++ front-end/src/components/HomeScreen.js
@@ -2,9 +2,7 @@ import React from 'react'
 // import { Nav, Navbar } from 'react-bootstrap';
 import Cards2 from './layout/Cards2'
 import {Cardss} from './layout/Cardss';
-import Footer from './layout/Footer/index'
-import {Nav} from "./layout/Nav/index"
-import {Navbar} from './layout/Navbar/index'
+
 import {Banner} from './layout/Banner'
 import {Search} from './layout/Search/index'
 import AboutUs from './layout/AboutUs'
