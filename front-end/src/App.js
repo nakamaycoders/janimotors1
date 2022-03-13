1ea0918c0d22fadb5c2d6786c265b7daa0d9b8c3
nakamaycoders <nakamaycoders@gmail.com>
2022-03-13 07:45:29 +0500

    zubair commit 6

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,19 +4,22 @@ import Home from "./components/Page/Home/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Calculator from "./components/Page/Financing/Calculator/Calculator";
 import ProductList from "./components/Page/ProductPage/ProductList";
-import ProductDetail from './components/Page/ProductPage/ProductDetail'
+import ProductDetail from "./components/Page/ProductPage/ProductDetail";
+import AboutUs from "./components/layout/Aboutus/AboutUs";
+import Footer from "./components/layout/Footer/index";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/:productSlug/:productId/p" component={ProductDetail} /> 
+        <Route path="/:productSlug/:productId/p" component={ProductDetail} />
         <Route path="/calculator" component={Calculator} />
         <Route path="/:slug" component={ProductList} />
-
       </Switch>
-    </BrowserRouter>
+      <AboutUs />
+      <Footer />
+    </>
   );
 }
 
