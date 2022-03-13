c1f1c0b6f2258657760f95f03227375c217b1565
nakamaycoders <83423016+nakamaycoders@users.noreply.github.com>
2022-03-13 07:50:26 +0500

    gee

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,9 +15,9 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/:productSlug/:productId/p" component={ProductDetail} /> 
         <Route path="/calculator" component={Calculator} />
-        <Route path="/:slug" component={ProductList} />
         <Route path="/tradeincar" component={TradeInCar} />
         <Route path="/creditapproval" component={Creditapproval} />
+        <Route path="/:slug" component={ProductList} />
 
       </Switch>
     </BrowserRouter>
