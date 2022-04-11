fa768c7de38fdf813a6d11619237afb740799b92
nakamaycoders <nakamaycoders@gmail.com>
2022-04-12 02:18:56 +0500

    Update JointForm.js

--- back-end/Controllers/JointForm.js
+++ back-end/Controllers/JointForm.js
@@ -2,165 +2,164 @@ const Joint = require("../models/JointForm");
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
-exports.createJointForm = catchAsyncErrors(async(req, res, next) => {
+exports.createJointForm = catchAsyncErrors(async (req, res, next) => {
   const {
-       fname ,
-       midName ,
-  lName ,
-  Suffix ,
-  homeNum ,
-  cellNum ,
-  email ,
-  Vemail ,
-  rr ,
-  box ,
-  street ,
-  StreetName ,
-  StreetOptional ,
-  apt ,
-  zip ,
-  city ,
-  State ,
-  House ,
-  Year ,
-  Month ,
-  Mortgage ,
-  Dob ,
-  SSN ,
-  CoApplicantRelation ,
-  SelectHousingStatus,
-  Employer,
-  WorkTitle,
-  WorkPhone,
-  yearss,
-  monthss ,
-  SelfWorkPhone,
-  Selfyear,
-  Selfmonths,
-  EmpStatus,
-  PerYear,
-//   <..................CO-applicant info.............................>
-  Cofname ,
-  CoMidName ,
-  ColName ,
-  CoSuffix ,
-  CocellNum ,
-  CohomeNum ,
-  Coemail ,
-  CoVemail ,
-
-//   <...........step2.........>
-  Corelease ,
-  Corr ,
-  Cobox ,
-  Costreet ,
-  CoStreetName ,
-  CoStreetOptional ,
-  Coapt ,
-  Cozip ,
-  Cocity ,
-  CoState ,
-  Corelease2 ,
-  CoHouse ,
-  CoYear ,
-  CoMonth ,
-  CoMortgage ,
-  Codob ,
-  CoSSN ,
-
-
-  CoSelectHousingStatus ,
-  CoEmployer ,
-  CoWorkTitle ,
-  CoWorkPhone ,
-  Coyearss ,
-  Comonthss ,
-  CoSelfWorkPhone ,
-  CoSelfyear ,
-  CoSelfemployer ,
-  CoSelfmonths ,
-  CoEmpStatus ,
-  CoPerYear ,
-   view } = req.body;
-
-  const joint = new Joint({
-    fname ,
-    midName ,
-    lName ,
-    Suffix ,
-    homeNum ,
-    cellNum ,
-    email ,
-    Vemail ,
-    rr ,
-    box ,
-    street ,
-    StreetName ,
-    StreetOptional ,
-    apt ,
-    zip ,
-    city ,
-    State ,
-    House ,
-    Year ,
-    Month ,
-    Mortgage ,
-    Dob ,
-    SSN ,
-    CoApplicantRelation ,
+    fname,
+    midName,
+    lName,
+    Suffix,
+    homeNum,
+    cellNum,
+    email,
+    Vemail,
+    rr,
+    box,
+    street,
+    StreetName,
+    StreetOptional,
+    apt,
+    zip,
+    city,
+    State,
+    House,
+    Year,
+    Month,
+    Mortgage,
+    Dob,
+    SSN,
+    CoApplicantRelation,
     SelectHousingStatus,
     Employer,
     WorkTitle,
     WorkPhone,
     yearss,
-    monthss ,
+    monthss,
+    SelfWorkPhone,
+    Selfyear,
+    Selfmonths,
+    EmpStatus,
+    PerYear,
+    //   <..................CO-applicant info.............................>
+    Cofname,
+    CoMidName,
+    ColName,
+    CoSuffix,
+    CocellNum,
+    CohomeNum,
+    Coemail,
+    CoVemail,
+
+    //   <...........step2.........>
+    Corelease,
+    Corr,
+    Cobox,
+    Costreet,
+    CoStreetName,
+    CoStreetOptional,
+    Coapt,
+    Cozip,
+    Cocity,
+    CoState,
+    Corelease2,
+    CoHouse,
+    CoYear,
+    CoMonth,
+    CoMortgage,
+    Codob,
+    CoSSN,
+
+    CoSelectHousingStatus,
+    CoEmployer,
+    CoWorkTitle,
+    CoWorkPhone,
+    Coyearss,
+    Comonthss,
+    CoSelfWorkPhone,
+    CoSelfyear,
+    CoSelfemployer,
+    CoSelfmonths,
+    CoEmpStatus,
+    CoPerYear,
+    view,
+  } = req.body;
+
+  const joint = new Joint({
+    fname,
+    midName,
+    lName,
+    Suffix,
+    homeNum,
+    cellNum,
+    email,
+    Vemail,
+    rr,
+    box,
+    street,
+    StreetName,
+    StreetOptional,
+    apt,
+    zip,
+    city,
+    State,
+    House,
+    Year,
+    Month,
+    Mortgage,
+    Dob,
+    SSN,
+    CoApplicantRelation,
+    SelectHousingStatus,
+    Employer,
+    WorkTitle,
+    WorkPhone,
+    yearss,
+    monthss,
     SelfWorkPhone,
     Selfyear,
     Selfmonths,
     EmpStatus,
     PerYear,
     // <..................CO-applicant info.............................>
-    Cofname ,
-    CoMidName ,
-    ColName ,
-    CoSuffix ,
-    CocellNum ,
-    CohomeNum ,
-    Coemail ,
-    CoVemail ,
-  
+    Cofname,
+    CoMidName,
+    ColName,
+    CoSuffix,
+    CocellNum,
+    CohomeNum,
+    Coemail,
+    CoVemail,
+
     // <...........step2.........>
-    Corelease ,
-    Corr ,
-    Cobox ,
-    Costreet ,
-    CoStreetName ,
-    CoStreetOptional ,
-    Coapt ,
-    Cozip ,
-    Cocity ,
-    CoState ,
-    Corelease2 ,
-    CoHouse ,
-    CoYear ,
-    CoMonth ,
-    CoMortgage ,
-    Codob ,
-    CoSSN ,
-  
-  
-    CoSelectHousingStatus ,
-    CoEmployer ,
-    CoWorkTitle ,
-    CoWorkPhone ,
-    Coyearss ,
-    Comonthss ,
-    CoSelfWorkPhone ,
-    CoSelfyear ,
-    CoSelfemployer ,
-    CoSelfmonths ,
-    CoEmpStatus ,
-    CoPerYear ,
+    Corelease,
+    Corr,
+    Cobox,
+    Costreet,
+    CoStreetName,
+    CoStreetOptional,
+    Coapt,
+    Cozip,
+    Cocity,
+    CoState,
+    Corelease2,
+    CoHouse,
+    CoYear,
+    CoMonth,
+    CoMortgage,
+    Codob,
+    CoSSN,
+
+    CoSelectHousingStatus,
+    CoEmployer,
+    CoWorkTitle,
+    CoWorkPhone,
+    Coyearss,
+    Comonthss,
+    CoSelfWorkPhone,
+    CoSelfyear,
+    CoSelfemployer,
+    CoSelfmonths,
+    CoEmpStatus,
+    CoPerYear,
     view,
   });
   joint.save((err, data) => {
@@ -176,60 +175,59 @@ exports.createJointForm = catchAsyncErrors(async(req, res, next) => {
   });
 });
 
-exports.getJointForm = catchAsyncErrors(async(req, res, next) => {
-    const JointInfo = await Joint.find();
+exports.getJointForm = catchAsyncErrors(async (req, res, next) => {
+  const JointInfo = await Joint.find();
 
-    res.status(201).json({
-      success: true,
-      JointInfo,
-    });
+  res.status(201).json({
+    success: true,
+    JointInfo,
   });
-  
+});
+
 exports.getJointDetailsById = catchAsyncErrors(async (req, res, next) => {
-    const { id } = req.params;
-    const result = await Joint.findOne({ _id: id });
-    if (!result) {
-      return next(new ErrorHander("Not found", 404));
-    }
-    res.status(200).json({
-      success: true,
-      result,
-    });
+  const { id } = req.params;
+  const result = await Joint.findOne({ _id: id });
+  if (!result) {
+    return next(new ErrorHander("Not found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    result,
   });
+});
 
 exports.deleteJointFormById = catchAsyncErrors(async (req, res, next) => {
-    const result = await Joint.findById(req.params.id);
-    if (!result) {
-      return next(new ErrorHander("Something Went Wrong", 400));
-    }
-  
-    await result.remove();
-  
-    res.status(201).json({
-      success: true,
-      message: "Deleted Successfully",
-    });
+  const result = await Joint.findById(req.params.id);
+  if (!result) {
+    return next(new ErrorHander("Something Went Wrong", 400));
+  }
+
+  await result.remove();
+
+  res.status(201).json({
+    success: true,
+    message: "Deleted Successfully",
   });
+});
 
 exports.updateDetails = catchAsyncErrors(async (req, res, next) => {
-    const updatedData = await Joint.findByIdAndUpdate(
-      req.params.id,
-      { view: "read" },
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
-    try {
-      res.status(200).json({
-        status: "Success",
-        data: {
-          updatedData,
-        },
-      });
-    } catch (err) {
-      console.log(err);
-      return next(new ErrorHander("Something Went Wrong", 400));
-
+  const updatedData = await Joint.findByIdAndUpdate(
+    req.params.id,
+    { view: "read" },
+    {
+      new: true,
+      runValidators: true,
     }
-  });
+  );
+  try {
+    res.status(200).json({
+      status: "Success",
+      data: {
+        updatedData,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return next(new ErrorHander("Something Went Wrong", 400));
+  }
+});
