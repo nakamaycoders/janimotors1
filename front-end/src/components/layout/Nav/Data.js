70e8125e6a767157dedffe8270b4c54421522b98
nakamaycoders <83423016+nakamaycoders@users.noreply.github.com>
2022-04-14 03:17:32 +0500

    updatedmmm

--- front-end/src/components/layout/Nav/Data.js
+++ front-end/src/components/layout/Nav/Data.js
@@ -1,10 +1,17 @@
 export const navmenu = [
     {
+        id:2,
+        title:'Location',
+        path:'/contactus',
+        icon: '<FontAwesomeIcon icon="fa-solid fa-phone" />',
+        cName:'nav-menues',
+    },
+
+    {
         id:1,
         title:'Contact Us',
         path:'/contactus',
         icon: '<FontAwesomeIcon icon="fa-solid fa-phone" />',
         cName:'nav-menues',
     },
-  
 ]
